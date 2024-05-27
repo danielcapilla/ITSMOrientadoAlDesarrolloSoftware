68e7dcafddff0f926ceadc5a1996c365a330dfe2
Daniel Capilla Sánchez <capilloski@gmail.com>
2024-05-27 13:00:34 +0200

    Class Proyecto y Class Contacto
    La clase Proyecto tiene todas sus variables correspondientes. La variable estado se ha realizado con un enum en vez de con otra clase. La variable color todavía no se como implementarla.

---
+++ src/main/java/com/tuempresa/itsmorientadoaldesarrollosoftware/run/Contacto.java
@@ -0,0 +1,10 @@
+package com.tuempresa.itsmorientadoaldesarrollosoftware.run;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+@Entity @Getter @Setter
+public class Contacto extends Identificable{
+
+}
--- src/main/java/com/tuempresa/itsmorientadoaldesarrollosoftware/run/Proyecto.java
+++ src/main/java/com/tuempresa/itsmorientadoaldesarrollosoftware/run/Proyecto.java
@@ -1,10 +1,65 @@
 package com.tuempresa.itsmorientadoaldesarrollosoftware.run;
 
+import java.time.*;
+
 import javax.persistence.*;
 
-import lombok.*;
+import org.openxava.annotations.*;
+import org.openxava.calculators.*;
 
+import lombok.*;
+@View(members=
+"nombre,color, icono;"
++ "descripcion;"
++ "estado;" 
++ "fechaDeInicio,fechaFinal;"
++ "clave;"
++ "asignarProyectosAUnContacto;"
++ "tags;"
+)
 @Entity @Getter @Setter
 public class Proyecto extends Identificable{
+	
+	@Column(length=50)
+	String descripcion;
+	
+	@Icon
+	@Column(length=40)
+	String icono;
+	
+	// De momento no se como hacerlo
+	
+	@Column(length=40)
+	String color;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(length = 15)
+	@Required
+	EstadoProyecto estado;
+	
+    @Required
+    @DefaultValueCalculator(CurrentLocalDateCalculator.class)
+    LocalDate fechaDeInicio;
+    
+    @Required
+    LocalDate fechaFinal;
+    
+    @Id @Column(length=9)
+    String clave;
+	 
+    @ManyToOne(fetch=FetchType.LAZY)
+    @DescriptionsList
+	Contacto asignarProyectosAUnContacto;
+    
+    // Mas adelante se tiene que cambiar (creo)
+    @Column(length=50)
+	String tags;
 
+}
+enum EstadoProyecto {
+    EN_PROCESO,
+    COMPLETADO,
+    CANCELADO,
+    ESPERANDO,
+    PRESUPUESTADO
 }
