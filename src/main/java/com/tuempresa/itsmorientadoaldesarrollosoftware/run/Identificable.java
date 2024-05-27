f2d3cf9bd41af1b7b58effe76d1b72cdfdf78418
Daniel Capilla Sánchez <capilloski@gmail.com>
2024-05-27 11:40:05 +0200

    Class Identificable y Class Proyecto
    La clase Identificable es una super clase que se va a usar para que hereden de ella tanto Proyecto como Contacto.

---
+++ src/main/java/com/tuempresa/itsmorientadoaldesarrollosoftware/run/Identificable.java
@@ -0,0 +1,21 @@
+package com.tuempresa.itsmorientadoaldesarrollosoftware.run;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.*;
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@MappedSuperclass // Marcada como una superclase mapeada en vez de como una entidad
+@Getter @Setter
+public class Identificable {
+ 
+    @Id @GeneratedValue(generator="system-uuid") @Hidden
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    @Column(length=32)
+    String oid; // oid para la clase maestra
+ 
+    @Column(length=50)
+    String nombre;
+}
