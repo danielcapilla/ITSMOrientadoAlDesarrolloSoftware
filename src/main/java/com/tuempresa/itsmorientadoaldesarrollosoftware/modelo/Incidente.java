464ec7203f5739ad9343b2b825cf86d69bb2f65b
Daniel Capilla Sánchez <capilloski@gmail.com>
2024-06-26 13:29:48 +0200

    Estados

--- src/main/java/com/tuempresa/itsmorientadoaldesarrollosoftware/modelo/Incidente.java
+++ src/main/java/com/tuempresa/itsmorientadoaldesarrollosoftware/modelo/Incidente.java
@@ -17,38 +17,51 @@ import lombok.*;
 @Entity @Getter @Setter
 public class Incidente {
 	
+
+	
 	
 	@Id @GeneratedValue(generator="system-uuid") @Hidden
     @GenericGenerator(name="system-uuid", strategy = "uuid")
     @Column(length=32)
     String oid;
+	
+	
 	@ManyToOne(fetch = FetchType.LAZY, optional = true)
-	@DescriptionsList(descriptionProperties="nombre",
+	@DescriptionsList(descriptionProperties="nombre, transicion",
 		    condition="${nombre} = ?",
 		    depends="estado")
 
 	@DefaultValueCalculator(value=EstadosInicial.class)
 	Estados estado;
 	
-	@PrePersist@PreUpdate
+	
+	@PostLoad
 	private void calcularEstado()
 	{
 		 System.out.print("CARGANDO"+"\n");
+		 System.out.print(estado.getNombre()+"\n");
 	        EntityManager entityManager = XPersistence.getManager();
 	        Query query = entityManager.createQuery("select e from Estados e");
 	        List<Estados> estados = query.getResultList();
 
 	        if (!estados.isEmpty()) {
-	            Estados primerEstado = estados.get(0); // Tomamos el primer estado como ejemplo
-	            if (estado.getEstadoPadre().isEmpty()) {
+	        	if (estado.getNombre().isEmpty()) {
 	            	System.out.print("SAAA" +"\n");
-	                this.estado = primerEstado;
-	            } else {
+	                this.estado = estados.get(0);;
+	                System.out.print(estado.getNombre()+"\n");
+	            }
+	        	else if (estado.getNombre().equals("ABIERTO"))
+	            {
+	            	System.out.print("SUUUI" +"\n");
+	            	this.estado = estados.get(1);
+	            	//this.estado = estados.get(2);
+	            	System.out.print(estados.size()+"\n");
+	            }
+	             else {
 	                System.out.print("SUUUI" +"\n");
 	            }
 	        }
+
 	}
-
-
 }
 
